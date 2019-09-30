644c0a2981d1d9fe1c00c621121de34439dc22af
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-30 13:08:16 -0700

    first get request is working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // implement your API here
 const express = require("express");
-
+const db = require("./data/db.js");
 const server = express();
 
+server.get("/api/users", (req, res) => {
+  db.find().then(users => res.status(200).json(users));
+});
 server.listen(8000, () => console.log("server is running!!!"));
